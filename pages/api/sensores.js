ccc7d6248df49e90a88583f9b82e0807fdc05823
Adrian <adricoque.coqa@gmail.com>
2024-03-08 12:12:53 -0600

    UPDATE cors methods

--- pages/api/sensores.js
+++ pages/api/sensores.js
@@ -4,7 +4,7 @@ import initMiddleware from '@/libs/init-middleware';
 
 const cors = initMiddleware(
     Cors({
-        methods: ['GET', 'POST']
+        methods: ['GET', 'POST', 'PATCH']
     })
 )
 
