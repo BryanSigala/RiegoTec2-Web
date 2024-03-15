921477b5cf9069c120eeb9a4e908d791046d5e48
RaymundoBaca <bproentrans@gmail.com>
2024-03-15 13:04:18 -0600

    Edición de globals.css front tipo Tec

--- pages/index.js
+++ pages/index.js
@@ -5,146 +5,114 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <div>
-      <style>{`
-        /* Estilos para el body */
-        body {
-            font-family: Arial, sans-serif;
-            margin: 0;
-            padding: 0;
-            background-color: #ffffff; /* blanco */
-            color: #1b396a; /* azul marino */
-        }
-
-        /* Estilos para el encabezado */
-        header {
-            background-color: #ffffff; /* azul marino */
-            color: #000000; /* blanco */
-            padding: 1em;
-            font-family: sans-serif;
-            text-align: center;
-            position: relative; /* Añadimos posición relativa al header */
-        }
-
-        /* Estilos para el logo izquierdo */
-        .logo-left {
-            position: absolute;
-            top: 3px; /* Ajusta la distancia desde la parte superior */
-            left: 5px; /* Ajusta la distancia desde la izquierda */
-            max-width: 100%; /* Logro responsivo */
-            height: auto; /* Mantiene la proporción original de la imagen */
-        }
-
-        /* Estilos para el logo derecho */
-        .logo-right {
-            position: absolute;
-            top: 3px; /* Ajusta la distancia desde la parte superior */
-            right: 5px; /* Ajusta la distancia desde la derecha */
-            max-width: 100%; /* Logro responsivo */
-            height: auto; /* Mantiene la proporción original de la imagen */
-        }
-
-        /* Estilos para la barra de navegación */
-        nav {
-            background-color: #003366; /* azul oscuro */
-            color: #ffffff; /* blanco */
-            padding: 1em;
-            text-align: center;
-        }
-
-        /* Estilos para la sección */
-        section {
-            padding: 2em;
-        }
-
-        /* Estilos para el pie de página */
-        footer {
-            background-color: #1b396a; /* azul marino */
-            color: #ffffff; /* blanco */
-            padding: 1em;
-            text-align: center;
-        }
-
-        /* Estilos para los botones */
-        button {
-            width: 30%;
-            height: 100%;
-            background-color: #dbdbdb;
-            color: #1b396a;
-            border: 3px solid;
-            border-radius: 1vw;
-            border-color: #1b396a;
-            font-family: Verdana, Geneva, Tahoma, sans-serif;
-            font-size: 1.5vw;
-        }
-
-        /* Estilos para el contenedor principal */
-        #conPrincipal {
-            margin-top: 10px;
-            width: 40%;
-            height: 80%;
-            background-color: #dbdbdb;
-            border: 3px solid;
-            border-radius: 1vw;
-            box-shadow: 2px 2px 20px #1b396a;
-        }
-
-        /* Estilos para el contenedor secundario */
-        #conS {
-            width: 100%;
-            height: 100%;
-        }
-
-        /* Estilos para el segundo contenedor secundario */
-        #conS2 {
-            width: 80%;
-            height: 100%;
-            text-align: left;
-        }
-      `}</style>
-      <header>
-        <h1>Sistema de riego automático</h1>
-        <p>Tecnológico Nacional de México Campus Chihuahua 2</p>
-      </header>
-      <nav>
-        {/* <a href="#">Inicio</a>
-        <a href="#">Acerca de</a>
-        <a href="#">Servicios</a>
-        <a href="#">Contacto</a> */}
-      </nav>
-      <section>
-        <h2>Bienvenido</h2>
-        <p>Este es un sitio web en el que se realizará el riego automatizado de las instalaciones del Instituto Nacional
-            de México campus chihuahua 2.</p>
-        <div id='conPrincipal'>
-          <div id='conS'>
-            <h1>Sistema de Riego ITCHII</h1>
-          </div>
-          <div id='conS'>
-            <p><a href='/on'><button>Regar</button></a></p>
-          </div>
-          <div id='conS'>
-            <p><a href='/off'><button>Suspender</button></a></p>
-          </div>
+    <main
+      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+    >
+      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+          Get started by editing&nbsp;
+          <code className="font-mono font-bold">pages/index.js</code>
+        </p>
+        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+          <a
+            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
+            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            By{" "}
+            <Image
+              src="/vercel.svg"
+              alt="Vercel Logo"
+              className="dark:invert"
+              width={100}
+              height={24}
+              priority
+            />
+          </a>
         </div>
-        <div id='conPrincipal'>
-          <div id='conS'>
-            <h1>Señales</h1>
-          </div>
-          <div id='conS2'>
-            <p>Humedad de aire: </p>
-          </div>
-          <div id='conS2'>
-            <p>Humedad de suelo: </p>
-          </div>
-          <div id='conS2'>
-            <p>Temperatura: </p>
-          </div>
-        </div>
-      </section>
-      <footer>
-        <p>&copy;Todos los derechos reservados.</p>
-      </footer>
-    </div>
+      </div>
+
+      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
+        <Image
+          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+          src="/next.svg"
+          alt="Next.js Logo"
+          width={180}
+          height={37}
+          priority
+        />
+      </div>
+
+      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+        <a
+          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            Docs{" "}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+            Find in-depth information about Next.js features and API.
+          </p>
+        </a>
+
+        <a
+          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            Learn{" "}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+            Learn about Next.js in an interactive course with&nbsp;quizzes!
+          </p>
+        </a>
+
+        <a
+          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            Templates{" "}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+            Discover and deploy boilerplate example Next.js&nbsp;projects.
+          </p>
+        </a>
+
+        <a
+          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            Deploy{" "}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
+            Instantly deploy your Next.js site to a shareable URL with Vercel.
+          </p>
+        </a>
+      </div>
+    </main>
   );
 }
