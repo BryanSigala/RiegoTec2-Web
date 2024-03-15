a66a52e4cb384c739724564c4a6d113ff1a42eda
Adrian <adricoque.coqa@gmail.com>
2024-03-15 11:04:45 -0600

    Update login placeholder

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -15,14 +15,14 @@ export default NextAuth({
     Credentials({
       name:  'Credentials',
       credentials: {
-        username: { label: "Username", type: "text", placeholder: "jsmith" },
+        username: { label: "Username", type: "text", placeholder: "administrador" },
         password: { label: "Password", type: "password" } 
       },
       async authorize(credentials, req) {
         const db = conn;
         const user = await db.query(`SELECT * FROM usuarios WHERE nombre='${credentials.username}' AND password='${credentials.password}'`);
         if (user[0].length > 0) {
-          return { id: user[0][0].idUsuario, name: user[0][0].username }
+          return { id: user[0][0].idUsuario, name: user[0][0].nombre }
         } else {
           return null
         }
