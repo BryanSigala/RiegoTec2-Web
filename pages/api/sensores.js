d141e9229d242658b4b00fb073ee45420d936017
Adrian <adricoque.coqa@gmail.com>
2024-03-22 12:28:15 -0600

    Fix sensores route

--- pages/api/sensores.js
+++ pages/api/sensores.js
@@ -1,8 +1,6 @@
 import {conn} from '@/libs/mysql'
 import Cors from 'cors'
 import initMiddleware from '@/libs/init-middleware';
-import { getServerSession } from "next-auth/next"
-import { authOptions } from "./auth/[...nextauth]"
 
 const cors = initMiddleware(
     Cors({
@@ -12,11 +10,6 @@ const cors = initMiddleware(
 
 export default async function handler(req, res){
     await cors(req,res)
-    const session = await getServerSession(req, res, authOptions)
-    if(!session){
-        res.status(401).json({error: 'No autorizado, inicie sesion primero'})
-        return
-    }else{
         if(req.method==='POST'){
             const sensores = await conn.query(`INSERT INTO sensores (humedadAire, temperatura, humedadSuelo, caudal, ciclosRiego)
             VALUES (${req.body.humedadAire}, ${req.body.temperatura}, ${req.body.humedadSuelo}, ${req.body.caudal}, ${req.body.ciclosRiego})`)
@@ -32,5 +25,4 @@ export default async function handler(req, res){
         else{
             res.status(405).json({error: 'Metodo no permitido'})
         }
-    }
 }
